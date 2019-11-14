825316fc3fa9888b28f4156785c1108d13505aaa
Urano Fumitaka <fumitala.urano@jp.ricoh.com>
2019-11-15 08:31:31 +0900

    create dc1_2.jar

--- dc/src/dc1_2/Main1_2.java
+++ dc/src/dc1_2/Main1_2.java
@@ -1,6 +1,6 @@
 package dc1_2;
 
-public class Main {
+public class Main1_2 {
 
 	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 		DigitalClock clock = new DigitalClock();
