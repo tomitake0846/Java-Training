0f7f5e6ce7cd4500b944a42cfe9c3df0cf0fc6c2
tomitake0846 <fredericab0846@gmail.com>
2020-12-19 14:07:31 +0900

    solved ch09-ex11

--- je8ri/src/ch09/ex11/CreditCardStealer.java
+++ je8ri/src/ch09/ex11/CreditCardStealer.java
@@ -1,10 +1,24 @@
 package ch09.ex11;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreditCardStealer {
 	public List<String> run() {
-		String[] command = {"cmd","/c","powershell","-Command",""};
+		//windows
+		String[] command = {"cmd","/c","powershell","\"ls -Recurse | sls -Pattern \\\"[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}\\\"\""};
 		Exploit exploiter = new Exploit();
+
+		try(InputStream is = exploiter.RCE(command);) {
+
+			return InputStreamFilter.read(is, Regex::isCreditCardFormat).stream()
+				.flatMap(s -> Regex.extractCreditCardNumbers(s).stream())
+				.collect(Collectors.toList());
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
